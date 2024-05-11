e83b2c7a4d019769bfc208a62132331f75ef397f
Likhhh <pavel.melckin@mail.ru>
2024-05-11 08:48:08 +0400

    Программа 2.0

--- Program.cs
+++ Program.cs
@@ -1,10 +1,31 @@
-﻿namespace ConsoleAppPR3
+﻿while (true)
 {
-    internal class Program
+    Console.WriteLine("Меню выбора: ");
+    Console.WriteLine("1. Выполнить операцию '+'");
+    Console.WriteLine("7. Выйти из программы");
+
+    Console.Write("Выберите действие (1-5): ");
+
+    int choice = int.Parse(Console.ReadLine());
+
+    switch (choice)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        case 1:
+            Console.WriteLine("Введите число А: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число В: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Результат: {a + b}");
+            break;
+
+        case 5:
+            Environment.Exit(0);
+            break;
+
+        default:
+            Console.WriteLine("Некорректный выбор. Попробуйте ещё раз.");
+            break;
     }
+    Console.WriteLine();
 }
